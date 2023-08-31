72f56ae37f46e25a5639bc5638ff430309a706df
ahma0 <dsd932@naver.com>
2023-08-31 09:20:04 +0900

    #1 프로젝트 생성

---
+++ src/main/java/ahma0/project/trepic/TrepicApplication.java
@@ -0,0 +1,13 @@
+package ahma0.project.trepic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrepicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrepicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ahma0/project/trepic/TrepicApplicationTests.java
@@ -0,0 +1,13 @@
+package ahma0.project.trepic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrepicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
